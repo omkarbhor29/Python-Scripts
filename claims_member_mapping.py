977c46acc2e1b49b62caa04567779e8d210c9286
OMKAR BHOR <45947877+omkarbhor29@users.noreply.github.com>
2024-04-25 03:16:27 -0700

    Add files via upload

--- claims_member_mapping.py
+++ claims_member_mapping.py
@@ -10,6 +10,10 @@ df = pd.read_excel(excel_file)
 directory = "D:\\Test\\Current 837"
 files_in_folder = os.listdir(directory)
 
+
+file_pattern1 = r"_Inst(\d+)"
+file_pattern2 = r"_Prof(\d+)"
+
 # Function to extract member ID from file
 def extract_member_id(line):
     match = re.search(r'NM1\*IL\*1\*.*?(\d{10}|0\d{9})~',line)
@@ -19,20 +23,42 @@ def extract_member_id(line):
         return None
 
 # Iterate through each row in excel and extract member Id
+
 for index,row in df.iterrows():
     file_name = row["837 File Name"]
-    if file_name in files_in_folder:
-        df.loc[index, 'File Exist'] = "Yes"
-    else:
-        df.loc[index, 'File Exist'] = "No"
-    file_path = os.path.join(directory,file_name)
-    if os.path.exists(file_path):
-        with open(file_path,'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                extract_number = extract_member_id(line)
-                if extract_number:
-                    df.loc[index,'Member ID'] = extract_number
+    match1 = re.search(file_pattern1,file_name)
+    match2 = re.search(file_pattern2,file_name)
+    if match1:
+        if file_name in files_in_folder:
+            df.loc[index, 'File Exist'] = "Yes"
+        else:
+            df.loc[index, 'File Exist'] = "No"
+        file_path = os.path.join(directory,file_name)
+        if os.path.exists(file_path):
+            with open(file_path,'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    extract_number = extract_member_id(line)
+                    modified_number = str(extract_number)
+                    MemberID = modified_number[:-2] + '-' + modified_number[-2:]
+                    if MemberID:
+                        df.loc[index, 'Member ID'] = MemberID
+
+    if match2:
+        if file_name in files_in_folder:
+            df.loc[index, 'File Exist'] = "Yes"
+        else:
+            df.loc[index, 'File Exist'] = "No"
+        file_path = os.path.join(directory,file_name)
+        if os.path.exists(file_path):
+            with open(file_path,'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    extract_number = extract_member_id(line)
+                    modified_number = str(extract_number)
+                    MemberID = modified_number[:-2] + '-' + modified_number[-2:]
+                    if MemberID:
+                        df.loc[index,'Member ID'] = MemberID
 
 
 # Save member ID in current Sheet
